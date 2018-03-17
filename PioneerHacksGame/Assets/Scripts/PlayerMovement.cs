501d0c3628465f6780a30d5c8e9cce37c03bbc43
Krish-Sankaran <ksankaran2107@gmail.com>
2018-03-17 14:32:27 -0700

    Click effects

--- PioneerHacksGame/Assets/Scripts/PlayerMovement.cs
+++ PioneerHacksGame/Assets/Scripts/PlayerMovement.cs
@@ -7,26 +7,40 @@ public class PlayerMovement : MonoBehaviour {
     public float hoverStrength;
     public float offSetFromZero;
     public float speed;
+    public float accel;
     public Camera cam;
     public NavMeshAgent agent;
+    public ParticleSystem destination;
+    private bool mouseDown = false;
+    private Vector3 emmitPos;
+    private ParticleSystem currentParticleSystem;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        GameObject.Instantiate(destination);
+    }
+
+    // Update is called once per frame
+    void Update () {
         transform.position = new Vector3(transform.position.x,(Mathf.Sin(Time.time)*hoverStrength)+offSetFromZero,transform.position.z);
+        
         if (Input.GetMouseButton(0)) { 
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit))
+            if(Physics.Raycast(ray, out hit) && mouseDown == false)
             {
+                GameObject.Destroy(currentParticleSystem);
                 agent.SetDestination(hit.point);
+                emmitPos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                destination.transform.position = emmitPos;
+                currentParticleSystem = GameObject.Instantiate(destination);
+                mouseDown = true;
             }
         }
+
+        if (Input.GetMouseButtonUp(0))
+            mouseDown = false;
 
         /*
         if (Input.GetKeyDown("w"))
