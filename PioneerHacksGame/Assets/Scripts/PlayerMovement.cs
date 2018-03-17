b0eaadec0eb618b18a2984ef00e0e2c6cb83080d
Neel <neelsankaran@gmail.com>
2018-03-17 11:39:11 -0700

    Added movement script

---
+++ PioneerHacksGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float hoverStrength;
+    public float offSetFromZero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x,(Mathf.Sin(Time.time)*hoverStrength)+offSetFromZero,transform.position.z);
+    }
+}
