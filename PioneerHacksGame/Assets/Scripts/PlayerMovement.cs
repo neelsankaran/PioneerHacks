96ecbd44ce2379860f5309e7e6255134e75a5f0c
Neel <neelsankaran@gmail.com>
2018-03-17 11:58:12 -0700

    Added some tuff to movement

--- PioneerHacksGame/Assets/Scripts/PlayerMovement.cs
+++ PioneerHacksGame/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float hoverStrength;
     public float offSetFromZero;
+    public float speed;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +15,22 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.position = new Vector3(transform.position.x,(Mathf.Sin(Time.time)*hoverStrength)+offSetFromZero,transform.position.z);
+        /*
+        if (Input.GetKeyDown("w"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward*speed);
+        }else if (Input.GetKeyDown("s"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.back * speed);
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+
+        }
+        */
     }
 }
