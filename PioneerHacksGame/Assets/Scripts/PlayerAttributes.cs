c57f218050f917ee5b723c95bf82b8cc9cb56aaf
sravangogulapti <sravangogulapati@gmail.com>
2018-03-17 16:35:23 -0700

    Fuel

--- PioneerHacksGame/Assets/Scripts/PlayerAttributes.cs
+++ PioneerHacksGame/Assets/Scripts/PlayerAttributes.cs
@@ -1,31 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttributes : MonoBehaviour {
-    private float fuel = 100;
+    private float fuel = 10;
     private float nextUpdate = 1;
+
+    public Text scoreText;
+    public Text gameOverText;
 
 	// Use this for initialization
 	void Start () {
-		
+        gameOverText.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Time.time >= nextUpdate)
         {
-            Debug.Log(Time.time + ">=" + nextUpdate);
+            //Debug.Log(Time.time + ">=" + nextUpdate);
             // Change the next update (current second+1)
             nextUpdate = Mathf.FloorToInt(Time.time) + 1;
             // Call your fonction
             fuel--;
+            if (fuel == 0)
+            {
+                gameOver();
+            }
         }
-        Debug.Log(fuel);
+        scoreText.text = "Fuel: " + fuel;
     }
 
     public void addFuel(float amountAdded)
     {
         fuel += amountAdded;
+    }
+
+    public void gameOver()
+    {
+        gameOverText.text = "Fuel Depleted";
+        gameObject.GetComponent<PlayerMovement>().enabled = false;
+        this.enabled = false;
+        gameOverText.gameObject.SetActive(true);
     }
 }
